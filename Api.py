afc307821e91109f498b72005e94a77b253e1d04
rodrigolb01 <rodrigolb01@gmail.com>
2021-11-14 18:54:18 -0300

    api para testes

---
+++ Api.py
@@ -0,0 +1,54 @@
+import json
+from flask import Flask, jsonify
+from Blockchain import Blockchain
+
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/', methods=['GET'])
+
+def main():
+    wl = '''
+    <html>
+    <head>Blockchain iteractive API Test</head>
+    <body>
+       <br></br>
+       <a href='mineblock'>mine</a>
+       <br></br>
+       <a href='chain'>go to chain</a>
+    </body> 
+     '''
+    return wl
+@app.route('/mineblock', methods=['GET'])
+
+def mineblock():
+    i = 0
+    while i< 100000000 :
+        # minerar novo block
+        prev_block = blockchain.previous_block()
+        prev_proof = prev_block['proof']
+        proof = blockchain.proof_of_work(prev_proof)
+        prev_hash = blockchain.hash(prev_block)
+
+        generated_block = blockchain.create_block( previous_hash=prev_hash, proof=proof)
+
+    # retornar uma resposta para o cliente (navegador ou Postman)
+    response = {'message': "Block_mined:",
+                'index': generated_block['index'],
+                'timestamp': generated_block['timestamp'],
+                'proof': generated_block['proof'],
+                'previous block': generated_block['previous_hash']}
+
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+
+def getchain():
+    response = {'chain': blockchain.chain,
+                'len': len(blockchain.chain)}
+    
+    return jsonify(response), 200
+
+# inicializa a api
+app.run(host='0.0.0.0', port=5000)
--- Blockchain.py
+++ Blockchain.py
@@ -1,19 +1,55 @@
-import datetime
-import requests
+import sys
+import time
 import json
+from hashlib import sha256
+from random import seed, randint
 
 class Blockchain:
     def __init__(self):
-        chain = []
-        self.create_block(previous="0", proof=1)# minerar o primeiro block da cadeia
+        self.current_transactions = []
+        self.chain = []
+        self.create_block(previous_hash="0", proof=1)# minerar o primeiro block da cadeia
 
-    def create_block(self, previous, proof):
+    def create_block(self, previous_hash, proof):
         block = {
-            "index" : len(self.chain),
-            "time" : str(datetime.datetime.now()),
-            "previous_hash" : previous,
-            "proof" : proof
+            "index": len(self.chain) + 1,
+            "timestamp": time.time(),
+            "transactions": self.current_transactions,
+            "previous_hash": previous_hash,
+            "proof": proof
         }
 
+        self.current_transactions = []
         self.chain.append(block)
         return block
+
+    def proof_of_work(self, last_proof):
+        print("computing1...")
+        seed()
+        proof = randint(0, 100)
+        print("computing2...")
+        while self.validate_proof(last_proof, proof) is False:
+            proof = randint(0, sys.maxsize)
+
+        return proof
+
+    @staticmethod
+    def validate_proof(last_proof, proof):
+        number_of_zeroes = 5
+        hash = sha256(str(proof * last_proof).encode()).hexdigest()
+        return hash[:5] == "0" * number_of_zeroes
+
+    def lenght(self):
+        return len(self.chain)
+
+    def previous_block(self):
+        return self.chain[self.lenght() -1]
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys= True).encode()
+        return sha256(encoded_block).hexdigest()
+
+
+
+
+    
